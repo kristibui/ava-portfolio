23274cf88ab36a409c3e4dd8f76e6e4f4828ae1c
kristibui <>
2022-01-24 12:14:19 -0500

    Remove period.

--- src/about/AboutComponent.js
+++ src/about/AboutComponent.js
@@ -21,7 +21,7 @@ const AboutComponent = () => {
                 href='https://camd.northeastern.edu/journalism-at-northeastern/'>Northeastern University School of Journalism</a> in 
                 Boston. I have a passion for investigative and audio journalism, and I 
                 previously helped produce a WNYC and NPR show 
-                called <a className='about-link' href='https://www.wnycstudios.org/podcasts/otm'>On the Media.</a>. As an intern 
+                called <a className='about-link' href='https://www.wnycstudios.org/podcasts/otm'>On the Media</a>. As an intern 
                 at the <a className='about-link' href='https://www.wsj.com/'>Wall Street Journal</a>, I contributed to an investigative 
                 series exposing corruption in the federal court system, and I later 
                 helped cover the Taliban’s 2021 capture of Kabul.
