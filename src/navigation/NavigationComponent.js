5c251695105cda5806d60128eaaa6c34d21ca710
Kristi Bui <kristibui@kristis-air.mynetworksettings.com>
2023-05-16 12:01:36 -0400

    red link hover

--- src/navigation/NavigationComponent.js
+++ src/navigation/NavigationComponent.js
@@ -12,23 +12,17 @@ const NavigationComponent = () => {
             </div>
 
             <div className='right-links'>
-                
                 <div className='portfolio-sub-links'>
-
-                <span className='nav-link portfolio-nav'>Portfolio</span>
+                <span className='hoverable-link nav-link portfolio-nav'>Portfolio</span>
                     <ul>
-						<li><a href="/read">Read</a></li>
-						<li><a href="/listen">Listen</a></li>
+						<li><a className='hoverable-link' href="/read">Read</a></li>
+						<li><a className='hoverable-link' href="/listen">Listen</a></li>
 					</ul>
                 </div>
 
-
-
-
-                <a className='nav-link' href="/about">About</a>
-                <a className='nav-link' href="/contact">Contact</a>
-
-                <a className='twitter-icon' href="https://twitter.com/AvaSasani">
+                <a className='nav-link hoverable-link' href="/about">About</a>
+                <a className='nav-link hoverable-link' href="/contact">Contact</a>
+                <a className='twitter-icon hoverable-link' href="https://twitter.com/AvaSasani" target="_blank" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faTwitter} size='2x'/>
                 </a>
             </div>
